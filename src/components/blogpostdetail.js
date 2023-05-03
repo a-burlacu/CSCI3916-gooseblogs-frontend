ef5ab73d9bcbc8ac2c28763157503f7aaf966e46
a-burlacu <alina.burlacu@ucdenver.edu>
2023-05-03 13:53:49 -0600

    updated blogpostdetail

--- src/components/blogpostdetail.js
+++ src/components/blogpostdetail.js
@@ -11,13 +11,12 @@ class BlogpostDetail extends Component {
 
         this.updateDetails = this.updateDetails.bind(this);
         this.submit = this.submit.bind(this);
-        this.updateRating = this.updateRating.bind(this);
+        // this.updateRating = this.updateRating.bind(this);
         this.state = {
             details:{
-                movieID: this.props.selectedMovie.title,
-                name: localStorage.getItem('username'),
-                quote: '',
-                rating: ''
+                blogpostTitle: this.props.selectedBlogpost.title,
+                username: localStorage.getItem('username'),
+                quote: ''
             }
         };
     }
@@ -33,77 +32,79 @@ class BlogpostDetail extends Component {
 
     componentDidMount() {
         const {dispatch} = this.props;
-        if (this.props.selectedMovie == null) {
-            dispatch(fetchMovie(this.props.title));
+        if (this.props.selectedBlogpost == null) {
+            dispatch(fetchBlogpost(this.props.title));
         }
     }
 
     submit(){
         const {dispatch} = this.props;
-        dispatch(submitReview(this.state.details));
+        dispatch(submitComment(this.state.details));
     }
 
-    updateRating(rating){
-        let updateDetails = Object.assign({}, this.state.details);
-
-        updateDetails.rating = rating.target.value;
-        this.setState({
-            details: updateDetails
-        });
-    }
+    // updateRating(rating){
+    //     let updateDetails = Object.assign({}, this.state.details);
+    //
+    //     updateDetails.rating = rating.target.value;
+    //     this.setState({
+    //         details: updateDetails
+    //     });
+    // }
 
     render() {
 
-        if (!this.props.selectedMovie) {
+        if (!this.props.selectedBlogpost) {
             return <div>Loading....</div>
         }
 
         return (
             <Card>
-                <Card.Header>Movie Detail</Card.Header>
+                <Card.Header>Blogpost Detail</Card.Header>
                 <Card.Body>
-                    <Image className="image" src={this.props.selectedMovie.imageURL} thumbnail />
+                    <Image className="image" src={this.props.selectedBlogpost.imageUrl} thumbnail />
                 </Card.Body>
                 <ListGroup>
-                    <ListGroupItem>{this.props.selectedMovie.title}</ListGroupItem>
+                    <ListGroupItem>{this.props.selectedBlogpost.title}</ListGroupItem>
                     <ListGroupItem>
-                        {this.props.selectedMovie?.actors?.map((actor, i) => {
-                            console.log(actor);
-                            return (<p key={i}>
-                                <b>{actor.actorName}</b> {actor.charName}
-                            </p>);
-                        })}
+                        {this.props.selectedBlogpost.postBody}
+                        {/*{this.props.selectedBlogpost?.actors?.map((actor, i) => {*/}
+                        {/*    console.log(actor);*/}
+                        {/*    return (<p key={i}>*/}
+                        {/*        <b>{actor.actorName}</b> {actor.charName}*/}
+                        {/*    </p>);*/}
+                        {/*})}*/}
                     </ListGroupItem>
-                    {/*<ListGroupItem><h4><BsStarFill/> {this.props.selectedMovie.avgRating}</h4></ListGroupItem>*/}
+                    {/*<ListGroupItem><h4><BsStarFill/> {this.props.selectedBlogpost.avgRating}</h4></ListGroupItem>*/}
                 </ListGroup>
                 <Card.Body>
-                    {this.props.selectedMovie?.movieReviews?.map((review, i) =>
+                    {this.props.selectedBlogpost?.blogpostComments?.map((comment, i) =>
                         <p key={i}>
-                            <b>{review.name}</b>&nbsp; {review.quote}
-                            &nbsp;  <BsStarFill /> {review.rating}
+                            <b>{comment.username}</b>&nbsp; {comment.quote}
+                            {/*&nbsp;  <BsStarFill /> {comment.rating}*/}
                         </p>
                     )}
                     <Form className='form-horizontal'>
                         <Form.Group controlId="quote">
-                            <Form.Label>Leave a Review:</Form.Label>
-                            <Form.Control onChange={this.updateDetails} value={this.state.details.quote} type="text" placeholder="Review" />
+                            <Form.Label>Leave a Comment:</Form.Label>
+                            <Form.Control onChange={this.updateDetails} value={this.state.details.quote} type="text" placeholder="Comment" />
                         </Form.Group>
 
-                        <Form.Group controlId="rating">
-                            <div key={'inline-radio'}>
-                                <Form.Label> Rate the Movie: &nbsp;
-                                    <Form.Check onChange={this.updateRating} inline label="1" name="rating" type="radio" value={1}/>
-                                    <Form.Check onChange={this.updateRating} inline label="2" name="rating" type="radio" value={2}/>
-                                    <Form.Check onChange={this.updateRating} inline label="3" name="rating" type="radio" value={3}/>
-                                    <Form.Check onChange={this.updateRating} inline label="4" name="rating" type="radio" value={4}/>
-                                    <Form.Check onChange={this.updateRating} inline label="5" name="rating" type="radio" value={5}/>
-                                    Stars </Form.Label>
-                            </div>
-                        </Form.Group>
-                        {console.log("movie", this.state.details.movieID)}
-                        {console.log("name", this.state.details.name)}
+                        {/*<Form.Group controlId="rating">*/}
+                        {/*    <div key={'inline-radio'}>*/}
+                        {/*        <Form.Label> Rate the Movie: &nbsp;*/}
+                        {/*            <Form.Check onChange={this.updateRating} inline label="1" name="rating" type="radio" value={1}/>*/}
+                        {/*            <Form.Check onChange={this.updateRating} inline label="2" name="rating" type="radio" value={2}/>*/}
+                        {/*            <Form.Check onChange={this.updateRating} inline label="3" name="rating" type="radio" value={3}/>*/}
+                        {/*            <Form.Check onChange={this.updateRating} inline label="4" name="rating" type="radio" value={4}/>*/}
+                        {/*            <Form.Check onChange={this.updateRating} inline label="5" name="rating" type="radio" value={5}/>*/}
+                        {/*            Stars </Form.Label>*/}
+                        {/*    </div>*/}
+                        {/*</Form.Group>*/}
+
+                        {console.log("blogpost", this.state.details.blogpostTitle)}
+                        {console.log("username", this.state.details.username)}
                         {console.log("quote", this.state.details.quote)}
-                        {console.log("rating", this.state.details.rating)}
+                        {/*{console.log("rating", this.state.details.rating)}*/}
                         <Button onClick={this.submit}>Submit</Button>
                     </Form>
                 </Card.Body>
@@ -114,7 +115,7 @@ class BlogpostDetail extends Component {
 
 const mapStateToProps = state => {
     return {
-        selectedMovie: state.movie.selectedMovie
+        selectedBlogpost: state.blogpost.selectedBlogpost
     }
 }
 
