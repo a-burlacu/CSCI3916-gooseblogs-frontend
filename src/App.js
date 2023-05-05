72826e297e733c809f35eaec47c97dad68d7e6bd
msalina007 <79892093+msalina007@users.noreply.github.com>
2023-05-04 22:13:52 -0600

    Changes by Alina

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import MovieHeader from './components/movieheader';
-import MovieList from './components/movielist';
-import Movie from './components/movie';
+import BlogpostHeader from './components/blogpostheader';
+import BlogpostList from './components/blogpostlist';
+import Blogpost from './components/blogpost';
 import Authentication from './components/authentication';
 import {HashRouter, Route} from 'react-router-dom';
 import { Provider } from 'react-redux';
