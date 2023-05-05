da20a012baa73843f0bee74b8fac6671cb9954d8
msalina007 <79892093+msalina007@users.noreply.github.com>
2023-05-04 22:20:00 -0600

    renamed vars

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import authReducer from "../reducers/authReducer";
-import movieReducer from "../reducers/blogpostReducer";
+import blogpostReducer from "../reducers/blogpostReducer";
 const middlewares = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === 'development') {
 const store = createStore(
     combineReducers( {
         auth: authReducer,
-        movie: movieReducer
+        blogpost: blogpostReducer
     }),
     applyMiddleware(
         ...middlewares
